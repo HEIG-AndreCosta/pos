950a970aeb8dc1e56401f8d941e9ed358ebd107d
André <andre_miguel_costa@hotmail.com>
2024-09-27 11:16:25 +0200

    feat: leds 1-4 driver

--- linux/usr/module/access.c
+++ linux/usr/module/access.c
@@ -8,19 +8,30 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/io.h>
 
-#include <asm/io.h>
-
-static int access_init(void) {
-
-	printk("access: small driver for accessing I/O ...\n");
+static void *led_ptr = NULL;
+static int access_init(void)
+{
+	printk("Hello!\n");
+	led_ptr = ioremap(0x2000003a, 8);
+	if (!led_ptr) {
+		printk("ERROR Remaping led memory\n");
+		return 1;
+	}
+	printk("Turning LEDS 1-4 ON\n");
+	writew(0x11E, led_ptr);
 
 	return 0;
 }
 
-static void access_exit(void) {
-
-	printk("access: bye bye!\n");
+static void access_exit(void)
+{
+	if (led_ptr) {
+		printk("Turning LEDS 1-4 OFF\n");
+		writew(0, led_ptr);
+	}
+	printk("Bye bye\n");
 }
 
 module_init(access_init);
@@ -28,4 +39,3 @@ module_exit(access_exit);
 
 MODULE_INFO(intree, "Y");
 MODULE_LICENSE("GPL");
-
