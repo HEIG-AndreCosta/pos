5179d35b2c5a584d77d00b29fe87b7c1af06d750
André <andre_miguel_costa@hotmail.com>
2024-10-04 09:53:28 +0200

    feat: vext emulation with led support

--- qemu/hw/arm/vext/vext.c
+++ qemu/hw/arm/vext/vext.c
@@ -17,13 +17,88 @@
  *
  */
 #include "vext.h"
+#include "exec/cpu-common.h"
+#include "exec/memory.h"
+#include "hw/sysbus.h"
+#include "qemu/typedefs.h"
+#include "qom/object.h"
 #include "vext_emul.h"
+#include <stdint.h>
+
+#define DEVICE_NAME "vext"
+typedef struct {
+	SysBusDevice busdev;
+	MemoryRegion iomem;
+
+	/*
+		7:0 LEDx RW 
+			write '1' to turn the LEDx ON, write '0' to turn it OFF
+		8 LED6_7_EN 
+			write '1' to control the LEDs 6 /7 from this register
+			write '0' to let the incremental encoder to control these LEDs
+		15:9 unassigned RW
+			read as '0'
+	*/
+	uint8_t leds;
+
+} vext_state_t;
 void vext_process_switch(void *opaque, cJSON *packet)
 {
 	/* to be completed ... */
 }
+static uint64_t vext_read(void *raw, hwaddr offset, unsigned size)
+{
+	vext_state_t *instance = (vext_state_t *)raw;
+	if (offset > 0) {
+		return 0;
+	}
+	return instance->leds;
+}
+static void update_frontend(vext_state_t *instance)
+{
+	cJSON *obj = cJSON_CreateObject(); /* Creation of the JSON object */
+	cJSON_AddStringToObject(obj, "device", "led");
+	cJSON_AddNumberToObject(obj, "value", instance->leds);
+	vext_cmd_post(obj);
+}
+static void vext_write(void *raw, hwaddr offset, uint64_t value, unsigned size)
+{
+	vext_state_t *instance = (vext_state_t *)raw;
+
+	if (offset > 0) {
+		return;
+	}
+	instance->leds = value & 0xFF;
+	update_frontend(instance);
+}
+static const MemoryRegionOps vext_ops = {
+	.read = vext_read,
+	.write = vext_write,
+	.endianness = DEVICE_NATIVE_ENDIAN,
+};
 static void vext_init(Object *obj)
 {
-	/* to be completed ... */
-	vext_emul_init(obj);
+	DBG("Initializing vext\n");
+	if (vext_emul_init(obj)) {
+		DBG("Error Initializing Object\n");
+		return;
+	}
+	DeviceState *dev = DEVICE(obj);
+	SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
+	vext_state_t *instance = OBJECT_CHECK(vext_state_t, dev, DEVICE_NAME);
+	memory_region_init_io(&instance->iomem, obj, &vext_ops, instance,
+			      DEVICE_NAME, 0x1000);
+	sysbus_init_mmio(sbd, &instance->iomem);
 }
+
+static const TypeInfo vext_info = { .name = DEVICE_NAME,
+				    .parent = TYPE_SYS_BUS_DEVICE,
+				    .instance_size = sizeof(vext_state_t),
+				    .instance_init = vext_init };
+
+static void vext_register_types(void)
+{
+	type_register_static(&vext_info);
+}
+
+type_init(vext_register_types)
