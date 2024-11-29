4e80e337bc9bedbcf154f1d720ff0c14e94d211a
André <andre_miguel_costa@hotmail.com>
2024-11-29 10:06:03 +0100

    feat: port event reader and simon to so3

---
+++ so3/usr/src/event_reader.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <linux/input.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+	struct input_event event;
+
+	if (argc < 2) {
+		printf("Usage %s <event_file>", argv[0]);
+		return EXIT_FAILURE;
+	}
+	int fd = open(argv[1], O_RDWR);
+
+	while (1) {
+		if (read(fd, &event, sizeof(event)) == -1) {
+			break;
+		}
+		printf("New event. Type: %d Code: %d Value: %d ", event.type,
+		       event.code, event.value);
+		switch (event.code) {
+		case KEY_ENTER:
+			printf("KEY_ENTER\n");
+			break;
+		case KEY_UP:
+			printf("KEY_UP\n");
+			break;
+		case KEY_DOWN:
+			printf("KEY_DOWN\n");
+			break;
+		case KEY_LEFT:
+			printf("KEY_LEFT\n");
+			break;
+		case KEY_RIGHT:
+			printf("KEY_RIGHT\n");
+			break;
+		default:
+			printf("Unkwnown code, %d\n", event.code);
+			break;
+		}
+	}
+	return EXIT_SUCCESS;
+}
---
+++ so3/usr/src/simon.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include <linux/input.h>
+#include <time.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <assert.h>
+#define NO_LEDS	  5
+#define BASE_PATH "/dev/vextled"
+
+const static int keys[] = { KEY_ENTER, KEY_LEFT, KEY_UP, KEY_RIGHT, KEY_DOWN };
+static int get_key_pressed(int event_fd);
+
+int get_key_pressed(int event_fd)
+{
+	struct input_event event;
+	int result = -1;
+
+	if (read(event_fd, &event, sizeof(event)) == -1) {
+		printf("Failed to read event file");
+		exit(EXIT_FAILURE);
+	}
+	assert(event.type == EV_KEY && event.value == 1);
+	result = event.code;
+
+	return result;
+}
+int main(int argc, char **argv)
+{
+	int led_fds[NO_LEDS];
+	int event_fd;
+	char file_path[50];
+	if (argc < 2) {
+		printf("Usage %s <event_file_path>", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	for (size_t i = 0; i < NO_LEDS; ++i) {
+		sprintf(file_path, BASE_PATH "%zu", i);
+		led_fds[i] = open(file_path, O_RDWR);
+		if (led_fds[i] == -1) {
+			printf("Couldn't open file %s\n", file_path);
+			return EXIT_FAILURE;
+		}
+	}
+
+	event_fd = open(argv[1], O_RDONLY);
+
+	printf("Welcome to Simon!\n");
+
+	for (size_t i = 0; i < NO_LEDS; ++i) {
+		write(led_fds[i], "1", sizeof("1"));
+	}
+	usleep(1000000);
+	for (size_t i = 0; i < NO_LEDS; ++i) {
+		write(led_fds[i], "0", sizeof("1"));
+	}
+
+	size_t round = 0;
+	size_t current_size = 10;
+	int *sequence = malloc(current_size * sizeof(int));
+	assert(sequence);
+	srand(time(NULL));
+	while (1) {
+		if (round == current_size) {
+			current_size *= 2;
+			sequence = realloc(sequence, current_size);
+			assert(sequence);
+		}
+
+		sequence[round++] = (rand() % (NO_LEDS - 1)) + 1;
+
+		for (int i = 0; i < round; ++i) {
+			const int curr_led = sequence[i];
+			usleep(500000);
+			write(led_fds[curr_led], "1", sizeof("1"));
+			usleep(500000);
+			write(led_fds[curr_led], "0", sizeof("1"));
+		}
+		usleep(200000);
+		write(led_fds[0], "1", sizeof("1"));
+		usleep(500000);
+		write(led_fds[0], "0", sizeof("1"));
+		for (int i = 0; i < round; ++i) {
+			const int curr_key = keys[sequence[i]];
+			const int key_pressed = get_key_pressed(event_fd);
+			if (key_pressed == curr_key) {
+				write(led_fds[curr_key], "1", sizeof("1"));
+				usleep(100000);
+				write(led_fds[curr_key], "0", sizeof("1"));
+			} else {
+				printf("Game Over. You made it to round %zu\n",
+				       round);
+				goto end;
+			}
+		}
+	}
+end:
+	free(sequence);
+	return 0;
+}
